e05b4e50d2846fc6a9c7e0c605ce458f08ec1ac0
BatoosaiRex <tu_correo@example.com>
2026-08-23 19:31:20 -0500

    Puse el archivo TXT

--- Test2.py
+++ Test2.py
@@ -10,3 +10,18 @@ print(df.columns.tolist())
 print("\nPrimeros valores por columna:")
 for col in df.columns:
     print(f"{col}: {df[col].head(3).tolist()}")
+
+
+
+import pandas as pd
+
+df = pd.read_excel(r"D:\Doc\OneDrive\Nokia\Archivo Carga.xlsx")
+
+# Mostrar nombres de columnas
+print("Columnas en el DataFrame:")
+print(df.columns.tolist())
+
+# Mostrar los primeros valores de cada columna
+print("\nPrimeros valores por columna:")
+for col in df.columns:
+    print(f"{col}: {df[col].head(3).tolist()}")
